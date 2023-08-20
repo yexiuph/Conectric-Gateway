8acab7c23821b77c66cda3c9cc7b8494e0c1d988
YeXiuPH <kirivps@gmail.com>
2023-08-20 23:58:01 +0200

    Added starting of gateway

--- conectric-gateway/src/main.rs
+++ conectric-gateway/src/main.rs
@@ -1,3 +1,10 @@
+use conectric_sdk::{
+    serial::{
+        start_gateway
+    }
+};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Conectric Gateway : RUST Edition");
+    start_gateway();
 }
