29ca9a7f82c32eefde2798cccb8ce3b92fc44e39
YeXiuPH <kirivps@gmail.com>
2023-08-24 06:34:18 +0200

    Added Debugging Capabilities

--- conectric-gateway/src/main.rs
+++ conectric-gateway/src/main.rs
@@ -2,5 +2,14 @@ use conectric_sdk::serial::ConectricSerial;
 
 fn main() {
     println!("Conectric Gateway : RUST Edition");
-    ConectricSerial::start_gateway();
+
+    let mut conectric_serial = ConectricSerial::new();
+    conectric_serial.start_gateway();
+
+    // Check if the serial port is opened
+    if let Some(serial_port) = &mut conectric_serial.serial_port {
+        serial_port.write(b"test\n").expect("Write failed!");
+    } else {
+        println!("Serial port is not opened.");
+    }
 }
